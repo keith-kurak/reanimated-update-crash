db172931f8ac01f6f88bb98f73fbdd6f558238a3
Keith Kurak <keith.kurak@gmail.com>
2023-02-09 16:47:54 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Button } from 'react-native';
+import * as Updates from 'expo-updates';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button title="Reload" onPress={Updates.reloadAsync} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
